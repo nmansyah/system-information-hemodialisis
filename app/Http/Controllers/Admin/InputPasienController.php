ac2a6037a50b440fe75cae70090bf5cdefa191a5
sam <samsulma828@gmail.com>
2020-02-09 10:09:50 +0700

    fix field insurance in data pasien

--- app/Http/Controllers/Admin/InputPasienController.php
+++ app/Http/Controllers/Admin/InputPasienController.php
@@ -14,67 +14,77 @@ use Auth;
 
 class InputPasienController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth');
-    $this->middleware('admin');
-  }
-  public function show()
-  {
-      $pasien= Pasien::all();
-      return view('admin/dataPasien',[
-      'pasien' => $pasien
-    ]);
-  }
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('admin');
+    }
+
+    public function show()
+    {
+        $pasien = Pasien::all();
+        return view('admin/dataPasien', [
+            'pasien' => $pasien
+        ]);
+    }
 
     public function index()
     {
-        return view('admin/inputPasien');
+        $insurances = ['BPI', 'BPJS', 'PRUDENTIAL', 'MANDIRI', 'Lain - lain'];
+        return view('admin/inputPasien', ['insurances' => $insurances]);
     }
 
     public function store(Request $request)
     {
 
-      $pasien = new Pasien;
+        $pasien = new Pasien;
+        $pasien->no_cm = $request->no_cm;
+        $pasien->nama = $request->nama;
+        $pasien->jenis_kelamin = $request->jenis_kelamin;
+        $pasien->alamat = $request->alamat;
+        $pasien->no_hp = $request->no_hp;
+        $pasien->usia = $request->usia;
+        $pasien->riwayat = $request->riwayat;
+        if ($request->asuransi == 'Lain - lain'){
+            if (is_null($request->asuransi_text)){
+                return redirect()->back()->with('alert', 'Field asuransi cant be blank');
+            }
+            $pasien->asuransi = $request->asuransi_text;
+        }else{
+            $pasien->asuransi = $request->asuransi;
+        }
+        $pasien->hari1 = $request->hari1;
+        $pasien->sesi1 = $request->sesi1;
+        $pasien->hari2 = $request->hari2;
+        $pasien->sesi2 = $request->sesi2;
+        $pasien->hari3 = $request->hari3;
+        $pasien->sesi3 = $request->sesi3;
+        if (!$pasien->save()){
+            return redirect()->back()->with('alert', 'Failed to store data');
+        }
 
-      $pasien->no_cm = $request->no_cm;
-      $pasien->nama = $request->nama;
-      $pasien->jenis_kelamin = $request->jenis_kelamin;
-      $pasien->alamat = $request->alamat;
-      $pasien->no_hp = $request->no_hp;
-      $pasien->usia = $request->usia;
-      $pasien->riwayat = $request->riwayat;
-      $pasien->asuransi = $request->asuransi;
-      $pasien->hari1 = $request->hari1;
-      $pasien->sesi1 = $request->sesi1;
-      $pasien->hari2 = $request->hari2;
-      $pasien->sesi2 = $request->sesi2;
-      $pasien->hari3 = $request->hari3;
-      $pasien->sesi3 = $request->sesi3;
-      $pasien->save();
-
-      return redirect()
-      ->route('admin.data.pasien')
-      ->withSuccess('Pasien Telah Ditambahkan.');
+        return redirect()
+            ->route('admin.data.pasien')
+            ->withSuccess('Pasien Telah Ditambahkan.');
     }
 
     public function delete($id)
     {
-      $pasien = DB::table('pasiens')->where('id', $id)->delete();
-      return redirect()
-      ->route('admin.data.pasien')
-      ->withSuccess('Dokter Berhasil Dihapuskan.');
+        $pasien = DB::table('pasiens')->where('id', $id)->delete();
+        return redirect()
+            ->route('admin.data.pasien')
+            ->withSuccess('Dokter Berhasil Dihapuskan.');
     }
 
     public function edit($id)
     {
-      $pasien = DB::table('pasiens')->where('id', $id)->first();
-      return view('/admin/editPasien', ['pasien' => $pasien]);
+        $pasien = DB::table('pasiens')->where('id', $id)->first();
+        return view('/admin/editPasien', ['pasien' => $pasien]);
     }
 
     public function update(Request $request, $id)
     {
-        $pasien = Pasien::where('id',$id)->first();
+        $pasien = Pasien::where('id', $id)->first();
         $pasien->no_cm = $request->no_cm;
         $pasien->nama = $request->nama;
         $pasien->usia = $request->usia;
@@ -90,7 +100,7 @@ class InputPasienController extends Controller
         $pasien->sesi3 = $request->sesi3;
         $pasien->save();
         return redirect()
-        ->route('admin.data.pasien')
-        ->with('alert-success','Data berhasil diubah!');
+            ->route('admin.data.pasien')
+            ->with('alert-success', 'Data berhasil diubah!');
     }
 }
