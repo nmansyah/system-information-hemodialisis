874f9d69dfd66034df75be8aae6faf348e770401
nurmansyah <nmansyah22@gmail.com>
2020-03-11 20:18:50 +0700

    unit in edit readonly

--- resources/views/admin/editPasienRawatinap.blade.php
+++ resources/views/admin/editPasienRawatinap.blade.php
@@ -42,7 +42,7 @@
                 <label class="control-label col-md-3 col-sm-3 col-xs-12" for="unit">Unit/Bangsal <span class="required">*</span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                <input id="unit" type="text" name="unit" class="optional form-control col-md-7 col-xs-12" value="{{ $pasien_rawatinap->unit }}">
+                <input id="unit" type="text" class="optional form-control col-md-7 col-xs-12" value="{{ $pasien_rawatinap->unit }}" readonly>
                 </div>
             </div>
             <div class="ln_solid"></div>
